2d06b31db4451a5708e8bfc659b9065da853fba8
Noah Nicholson <noahnicholson239@yahoo.com>
2022-06-03 11:15:49 -0500

    birthday function

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 one line of code
+const birthdays = () => {
+    console.log('Joe was bord June 22')
+    console.log('David was born Decemeber 1')
+    console.log('Noah was born December 1')
+    console.log('Haley was born May 9')
+    console.log('Jordan was born January 29')
+}
+birthdays();
