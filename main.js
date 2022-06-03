57ea991f0eb7d2fc97374a0049f091e40bcb2ccc
Haley Hickey <hchickey96@gmail.com>
2022-06-03 11:11:06 -0500

    pet function

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-one line of code
+
+//one line of code
+
+const petFun = () => {
+    console.log('Joe has a pet pittie named Bacon.')
+    console.log('David has a pet dog named Nash.')
+    console.log('Noah has a pet cat named Stewart.')
+    console.log('Haley has a pet dog named Willow.')
+    console.log('Jordan has no pets and is sad and lonely...jk he cool.')
+}
+petFun();
