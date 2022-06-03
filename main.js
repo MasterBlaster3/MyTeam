4594e5324b00c62d0767477fc4438d969b02cd78
Jordan-Cox92! <mrjordancolbycox@gmail.com>
2022-06-03 12:30:32 -0400

    log teammates

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-one line of code
+//Writes a function that console.logs everyone's names, one name per line
+
+teammateNames = () => {
+    console.log('Joe Maifield');
+    console.log('David Stewart');
+    console.log('Haley Hickey');
+    console.log('Noah Nicholson');
+    console.log('Jordan Cox');
+}
+
+teammateNames();
+
+
