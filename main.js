69ebd68867f16d9b6481323229f76f3d96ae8e6f
David Stewart <mboro19@gmail.com>
2022-06-03 13:07:43 -0500

    teammate facts, initial commit

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-one line of code
+funFacts = () => {
+
+    console.log( 'Jordan has talked to Neil DeGrasse Tyson for an hour.'),
+    console.log('Joe has made the szechuan sauce from Rick and Morty.'),
+    console.log('Haley got interviewed on a spanish radio station in Chile.'),
+    console.log( 'Noah astonishingly has no fun facts.'),
+    console.log('David has competed in multiple SPL competitions.')
+
+}
+funFacts();
